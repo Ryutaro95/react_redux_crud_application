fe0d6b6a172c8044a37cadacf8f940682ecf5cd8
Ryutaro95 <ryutaro9595@gmail.com>
2020-11-21 03:25:55 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,34 @@
 import React, { Component } from 'react';
 
-// AppクラスがComponentを継承している
-// Appのことをクラスコンポーネントと呼ぶ
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={ () => {console.log("I am clicked.")} }/>;
 
-//       </div>
-//     );
-//   }
-// }
-
-// 関数コンポーネント
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "NoName"}
+  ]
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
-      <Cat />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+      {/* nameというprops(属性)を与えた */}
+      {/* <User name={"Taro"} age={10} />
+      <User name={"Hanako"} age={5} /> */}
     </div>
   ); 
 }
 
-const Cat = () => {
-  return <div>Meow!</div>;
+// props を渡す
+const User = (props) => {
+return <div>Hi, I am {props.name}, and {props.age} years old!</div>;
+}
+
+// デフォルトでageに値を渡しておくことで、ageが存在しないときデフォルト値が設定される
+User.defaultProps = {
+  age: 1
 }
 
 export default App; 
